41352e29d7348defcbf315e75a02f61c65f370aa
李巧雯 <chiaowen@liqiaowens-MacBook-Air.local>
2017-08-27 11:32:59 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+import json, time, pprint
+import bot
+from collections import defaultdict
+from flask import Flask, request, make_response, render_template, jsonify
+
+
+pp = pprint.PrettyPrinter(indent=4)
+pyBot = bot.Bot()
+target_channel_id = ""
+
+app = Flask(__name__)
+
+@app.route("/begin_auth", methods=["GET"])
+def preInstall():
+    client_id = pyBot.oauth["client_id"]
+    oauth_scope = pyBot.oauth["scope"]
+    return render_template("install.html", scope=oauth_scope, client_id=client_id)
+
+@app.route("/finish_auth", methods=["GET", "POST"])
+def postInstall():
+    """
+    Retrieve the auth code from the request params and request the auth tokens from Slack.
+    """
+    auth_code = request.args['code']
+    pyBot.auth(auth_code)
+    return render_template("thanks.html")
+
+@app.route("/event_listening", methods=["GET", "POST"])
+def eventListening():
+    """
+    This route listens for incoming events from Slack and uses the event
+    handler helper function to route events to our Bot.
+    """
+    slack_event = json.loads(request.data)
+    print('slack_event =')
+    pp.pprint(slack_event)
+
+    # ============= Slack URL Verification ============ #
+    # In order to verify the url of our endpoint, Slack will send a challenge
+    # token in a request and check for this token in the response our endpoint
+    # sends back.
+    #       For more info: https://api.slack.com/events/url_verification
+    if "challenge" in slack_event:
+        return make_response(slack_event["challenge"], 200,
+                             {"content_type": "application/json"})
+
+    # ============ Slack Token Verification =========== #
+    # We can verify the request is coming from Slack by checking that the
+    # verification token in the request matches our app's settings
+    if pyBot.verification != slack_event.get("token"):
+        message = "Invalid Slack verification token: %s \npyBot has: \
+                   %s\n\n" % (slack_event["token"], pyBot.verification)
+        # By adding "X-Slack-No-Retry" : 1 to our response headers, we turn off
+        # Slack's automatic retries during development.
+        make_response(message, 403, {"X-Slack-No-Retry": 1})
+
+    # ====== Process Incoming Events from Slack ======= #
+    if "event" in slack_event:
+        event_type = slack_event["event"]["type"]
+        return _event_handler(event_type, slack_event)
+
+    # If our bot hears things that are not events we've subscribed to,
+    # send a quirky but helpful error response
+    return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
+                         you're looking for.", 404, {"X-Slack-No-Retry": 1})
+
+@app.route("/command_listening", methods=["POST"])
+def commandListening():
+    # get the key/value pairs in the body from a HTML post form
+    token = request.form.get("token", None)
+    command = request.form.get("command", None)
+    text = request.form.get("text", None)
+    user_id = request.form.get("user_id", None)
+    user_name = request.form.get("user_name", None)
+    channel_id = request.form.get("channel_id", None)
+    DMchannel_id = getDMChannelId(user_id) # direct message channel id
+
+    if command == "/truth":
+        target_channel_id = channel_id
+        post_message = pyBot.client.api_call("chat.postMessage",
+                            channel=channel_id,
+                            as_user=True,
+                            text="%s used `/truth`"
+                            )
+        if not post_message["ok"]:
+            pp.pprint(post_message)
+
+        # Send direct message to each member in the channel
+        all_members = getMembers(channel_id)
+        attachment_members = [{"text": memb, "value": memb} for memb in all_members.keys()]
+        for member_id in all_members.values():
+            post_message = pyBot.client.api_call("chat.postMessage",
+                                channel=getDMChannelId(member_id),
+                                as_user=True,
+                                text="Who is the man you want to kick out of your team?:smiling_imp:",
+                                attachments=[
+                                    {
+                                        "fallback": "Please update your Slack client.",
+                                        "attachment_type": "default",
+                                        "callback_id": "members_selection",
+                                        "actions": [
+                                            {
+                                                "name": "members_list",
+                                                "text": "Select a member...",
+                                                "type": "select",
+                                                "options": attachment_members
+                                            }
+                                        ]
+                                    }
+                                ]
+                                )
+            if not post_message["ok"]:
+                pp.pprint(post_message)
+    return make_response("", 200,)
+
+@app.route("/click_listening", methods=["POST"])
+def clickListening():
+    # Parse the request payload
+    data = json.loads(request.form["payload"])
+    actions = data["actions"]
+    original_message = data.get("original_message")
+    user_name = data["user"]["name"]
+    # pp.pprint(original_message)
+
+    if actions[0]["name"] == "members_list" & target_channel_id != "":
+        selected_option = actions[0]["selected_options"][0]["value"]
+        message = "Sorry @%s, @%s wants you to leave:sob:" % (selected_option, user_name)
+        post_message = pyBot.client.api_call("chat.postMessage",
+                            channel=target_channel_id,
+                            as_user=True,
+                            text=message,
+                            )
+        if not post_message["ok"]:
+            pp.pprint(post_message)
+    return make_response("", 200,) 
+
+def _event_handler(event_type, slack_event):
+    team_id = slack_event["team_id"]
+    event_time = slack_event["event"]["ts"]
+    channel_id = slack_event["event"]["channel"]
+    subtype = slack_event["event"].get("subtype")
+    user_id = slack_event["event"].get("user")
+    bot_id = slack_event["event"].get("bot_id")
+    text = slack_event["event"].get("text")
+    # ============== Message Events ============= #
+    if event_type == "message":
+        if user_id and text:
+            print("%s: %s [%s]" % (user_id, text, toDatetime(event_time)))
+
+    return make_response("", 200,)
+
+def getDMChannelId(user_id):
+    """
+    Get the direct message channel id of someone for the calling user (bot).
+    """
+    if not user_id:
+        return None
+    res = pyBot.client.api_call("im.list")
+    if res["ok"]:
+        for im in res["ims"]:
+            if im["user"] == user_id:
+                return im["id"]
+    else:
+        print("[Error in getDMChannelId: %s]" % res["error"])
+    return None
+
+def getUserName(user_id):
+    if not user_id:
+        return None
+    res = pyBot.client.api_call("users.info", user=user_id)
+    if res["ok"]:
+        return res["user"]["name"]
+    else:
+        print("[Error in getUserName: %s]" % res["error"])
+        return None
+
+def getMembers(channel_id):
+    if not channel_id:
+        return {}
+    res = pyBot.client.api_call("channels.info", channel=channel_id)
+    if res["ok"]:
+        members = {getUserName(user_id): user_id for user_id in res["channel"]["members"]}
+        return members
+    else:
+        print("[Error in getMembers: %s]" % res["error"])
+        return {}
+
+def toDatetime(unix_ts):
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(unix_ts)))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ bot.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Python Slack Bot class for use with the simpleBot app
+"""
+import os
+from slackclient import SlackClient
+
+
+# To remember which teams have authorized your app and what tokens are
+# associated with each team, we can store this information in memory on
+# as a global object. When your bot is out of development, it's best to
+# save this in a more persistant memory store.
+authed_teams = {}
+
+
+class Bot(object):
+    """ Instanciates a Bot object to handle Slack onboarding interactions."""
+    def __init__(self):
+        super(Bot, self).__init__()
+        self.name = "simplebot"
+        # When we instantiate a new bot object, we can access the app
+        # credentials we set earlier in our local development environment.
+        self.oauth = {"client_id": os.environ.get("SLACK_CLIENT_ID"),
+                      "client_secret": os.environ.get("SLACK_CLIENT_SECRET"),
+                      # Scopes provide and limit permissions to what our app
+                      # can access. It's important to use the most restricted
+                      # scope that your app will need.
+                      "scope": "bot"}
+        self.verification = os.environ.get("SLACK_VERIFICATION_TOKEN")
+
+        # NOTE: Python-slack requires a client connection to generate
+        # an oauth token. We can connect to the client without authenticating
+        # by passing an empty string as a token and then reinstantiating the
+        # client with a valid OAuth token once we have one.
+        # self.client = SlackClient(os.environ.get("SLACK_BOT_TOKEN"))
+        self.client = SlackClient("")
+        # We'll use this dictionary to store the state of each message object.
+        # In a production envrionment you'll likely want to store this more
+        # persistantly in a database.
+        self.messages = {}
+
+    def auth(self, code):
+        """
+        Authenticate with OAuth and assign correct scopes.
+        Save a dictionary of authed team information in memory on the bot
+        object.
+
+        Parameters
+        ----------
+        code : str
+            temporary authorization code sent by Slack to be exchanged for an
+            OAuth token
+
+        """
+        # After the user has authorized this app for use in their Slack team,
+        # Slack returns a temporary authorization code that we'll exchange for
+        # an OAuth token using the oauth.access endpoint
+        auth_response = self.client.api_call(
+                                "oauth.access",
+                                client_id=self.oauth["client_id"],
+                                client_secret=self.oauth["client_secret"],
+                                code=code
+                                )
+
+        # Save the bot token to an environmental variable or to your data store
+        # for later use
+        os.environ["SLACK_USER_TOKEN"] = auth_response['access_token']
+        os.environ["SLACK_BOT_TOKEN"] = auth_response['bot']['bot_access_token']
+        
+        # To keep track of authorized teams and their associated OAuth tokens,
+        # we will save the team ID and bot tokens to the global
+        # authed_teams object
+        team_id = auth_response["team_id"]
+        authed_teams[team_id] = {"bot_token":
+                                 auth_response["bot"]["bot_access_token"]}
+        # Then we'll reconnect to the Slack Client with the correct team's
+        # SLACK_BOT_TOKEN
+        self.client = SlackClient(authed_teams[team_id]["bot_token"])
+
+
